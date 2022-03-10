7cf615197d9d1a579d40c567c78e00302fffb32f
PeterDevLATAM <peter.devgrowth@gmail.com>
2022-03-10 18:45:29 -0500

    adedd React router v6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import './App.css';
-import HomePage from './pages/HomePage/HomePage.jsx'
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage.jsx";
 
 function App() {
   return (
-    <HomePage/>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
   );
 }
 
--- src/components/menu-item/MenuItem.jsx
+++ src/components/menu-item/MenuItem.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import "./menu-item.styles.scss";
+import { Link } from "react-router-dom";
 
-export default function MenuItem({ title, imageUrl, size }) {
+export default function MenuItem({ title, imageUrl, size, linkUrl }) {
   return (
-    <div className={`${size} menu-item`}>
+    <Link to={`/${linkUrl}`} className={`${size} menu-item`}>
       <div
         className="background-image"
-        style={{ backgroundImage: `url(${imageUrl})`}}
+        style={{ backgroundImage: `url(${imageUrl})` }}
       />
       <div className="content">
         <div className="title">{title.toUpperCase()}</div>
         <span className="subtitle">SHOP NOW</span>
       </div>
-    </div>
+    </Link>
   );
 }
