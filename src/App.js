64d06c583c618f57d652df6e3c084bbbd3edf7fc
PeterDevLATAM <peter.devgrowth@gmail.com>
2022-03-11 11:24:56 -0500

    Header component and some styling

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage.jsx";
 import ShopPage from "./pages/ShopPage/ShopPage";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
+    <>
+    <Header/>
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/shop" element={<ShopPage />} />
     </Routes>
+    </>
   );
 }
 
